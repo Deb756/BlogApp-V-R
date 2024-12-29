4763bbf55a83b58775127e2ae9d2646812b9aafc
Debabrat <123225627+Deb756@users.noreply.github.com>
2024-12-29 09:15:28 +0530

    Explore Page Added

---
+++ src/Component/Explore.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function Explore() {
+  return (
+    <>
+    <Navbar />
+    <div className="container my-4">
+
+    </div>
+    </>
+  )
+}
